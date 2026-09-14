701fdff3448013303d9c2f08dddd384b8a6db9c6
Sai Adhithiya <saiadhithiya13@gmail.com>
2026-09-14 19:07:14 +0530

    feat: add Self Dividing Numbers solution

--- algorithms/self-dividing-numbers/solution.java
+++ algorithms/self-dividing-numbers/solution.java
@@ -1,19 +1,30 @@
-import java.math.BigInteger;
-
 class Solution {
-    public int findGCD(int[] nums) {
-        int n = nums.length;
-        int min = nums[0];
-        int max = nums[0];
-        for(int i = 1; i < n; i++) {
-            if(nums[i] <min)
-                min = nums[i];
-        
-        if(nums[i] > max)
-            max = nums[i];
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> res = new ArrayList<>();
+        boolean valid;
+        int n;
+        for(int i = left; i <= right; i++) {
+            int temp = i;
+            valid = true;
+            int length = String.valueOf(Math.abs(i)).length();
+            for(int j = 0; j < length; j++) {
+                n = temp % 10;
+
+            if (n == 0) {
+                valid = false;
+                break;
+            }
+            
+                if(i % n != 0) {
+                    valid = false;
+                    break;
+            }
+                temp = temp/10;
+            }
+            if(valid) 
+                res.add(i);
         }
-        int gcd = BigInteger.valueOf(min).gcd(BigInteger.valueOf(max)).intValue();
-    
-        return gcd;
+        
+        return res;
     }
 }
