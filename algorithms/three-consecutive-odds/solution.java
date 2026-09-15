2f982b69acd34b794791a2fb58096c5a8937c409
Sai Adhithiya <saiadhithiya13@gmail.com>
2026-09-15 22:40:52 +0530

    feat: add Three Consecutive Odds solution

--- algorithms/three-consecutive-odds/solution.java
+++ algorithms/three-consecutive-odds/solution.java
@@ -1,13 +1,17 @@
 class Solution {
-    public String firstPalindrome(String[] words) {
-    int n = words.length;
-    
-    for(int i = 0; i < n; i++) {
-        String word = words[i];
-        if(word.equals(new StringBuilder(word).reverse().toString())) {
-        return word;
-    }
-    }
-    return "";
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int n = arr.length;
+        boolean res = false;
+        
+        if(n < 3) {
+            return false;
+        }
+
+        for(int i = 2; i < n ; i++) {
+            if(arr[i] % 2 != 0 && arr[i - 1] % 2 != 0 && arr[i - 2] % 2 != 0) {
+                res = true;
+            }
+        }
+        return res;
     }
 }
